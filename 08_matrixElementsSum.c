7924b7e86494a21334f6cca744bd44aed2c52f5f
omar13essam <22545687+omar13essam@users.noreply.github.com>
2019-02-21 10:11:25 +0200

    Create 08_matrixElementsSum.c

---
+++ 08_matrixElementsSum.c
@@ -0,0 +1,14 @@
+int matrixElementsSum(std::vector<std::vector<int>> matrix) {
+    int sum=0 ;
+    for (int i=0 ; i < matrix.size() ; i++ ){
+        for (int j=0 ; j < matrix[0].size() ; j++ ){
+            if ( i == 0 ) sum = sum + matrix[i][j];
+            else if (matrix[i-1][j] == 0){
+                matrix[i][j] = 0;
+                sum =sum + matrix[i][j];
+            }
+            else sum =sum + matrix[i][j];
+        }
+    }
+    return sum;
+}
